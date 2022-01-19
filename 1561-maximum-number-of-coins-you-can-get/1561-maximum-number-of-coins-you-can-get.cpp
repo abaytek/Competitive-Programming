ad1d1c7102438246e71c7af20fee13c9dfb78ea5
Abaysew Tekle <82055388+Abaysewtekle@users.noreply.github.com>
2022-01-19 21:41:00 +1100

    Time: 155 ms (38.04%), Space: 53.5 MB (58.09%) - LeetHub

--- 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.cpp
+++ 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.cpp
@@ -5,10 +5,14 @@ public:
         // 1 2 3 4 5 6 7 8 9
         sort(piles.begin(), piles.end());
         int result = 0;
-        int len = piles.size();
-        for(int i=len/3; i<len; i+=2)
+        int j = piles.size()-1;
+        int i = 0;
+        while(i <= j)
         {
-            result += piles[i];
+            j--;
+            result += piles[j];
+            j--;
+            i++;
         }
         return result;
     }
