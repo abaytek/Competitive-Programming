b973485edc8fd15cffcacdb0a25023405bb38fc6
=abaysew <abaysewtekle@gmail.com>
2023-02-02 09:54:03 -0800

    Solution for validate stack sequence with python

---
+++ validateStackSequence.py
@@ -0,0 +1,12 @@
+class Solution:
+    def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
+        stack=[]
+        i=0
+        for num in pushed:
+            stack.append(num) 
+            while  len(stack) > 0 and stack[len(stack)-1] == popped[i] :
+                stack.pop()
+                i+=1 
+        if len(stack) == 0: 
+            return True 
+        else: return False
