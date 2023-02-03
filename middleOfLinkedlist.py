9b0e1ba8511de8bebd3ea5ac996cdb906288dc3c
=abaysew <abaysewtekle@gmail.com>
2023-02-03 12:58:29 -0800

    Middle of linkedlist using two pointer

---
+++ middleOfLinkedlist.py
@@ -0,0 +1,15 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        fast, slow = head, head
+        if head == None:
+            return 
+        while fast != None and fast.next != None:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+        
